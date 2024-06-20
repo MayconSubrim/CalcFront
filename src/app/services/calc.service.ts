e8d2672870dffa4e0769ddb1ddd44a1953ade865
Maycon <mayconsubrim2002@gmail.com>
2024-06-20 02:04:19 -0300

    implementa auth basic no calcService

--- src/app/services/calc.service.ts
+++ src/app/services/calc.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -13,7 +13,14 @@ export class CalcService {
 
   private performOperation(endpoint: string, num1: number, num2: number): Observable<{ result: number }> {
     const data = { a: num1, b: num2 };
-    return this.http.post<{ result: number }>(`${this.baseUrl}/${endpoint}`, data)
+    const username = 'admin';
+    const password = 'password123';
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + btoa(`${username}:${password}`)
+    });
+
+    return this.http.post<{ result: number }>(`${this.baseUrl}/${endpoint}`, data, { headers })
       .pipe(
         catchError(error => {
           console.error('Erro ao realizar a operação:', error);
